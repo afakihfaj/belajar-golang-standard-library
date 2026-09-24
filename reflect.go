d48d3ec72c21221589d3a3abd4a5351f59dd66dc
afakihfaj <afakihfaj@gmail.com>
2026-09-24 23:20:14 +0700

    feat : mempelajari package reflect

---
+++ reflect.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type Sample struct {
+	Name string `required:"true" max:"10"`
+}
+
+type Person struct {
+	Name    string `required:"true" max:"10"`
+	Address string `required:"true" max:"10"`
+	Email   string `required:"true" max:"10"`
+}
+
+func readField(value any) {
+	var valueType reflect.Type = reflect.TypeOf(value)
+
+	fmt.Println("Type Name", valueType.Name())
+	for i := 0; i < valueType.NumField(); i++ {
+		var structField reflect.StructField = valueType.Field(i)
+		fmt.Println(structField.Name, "with type", structField.Type)
+		fmt.Println(structField.Tag.Get("required"))
+		fmt.Println(structField.Tag.Get("max"))
+
+	}
+}
+
+func main() {
+	readField(Sample{"Afakih"})
+	readField(Person{"Afakih", "Situbondo", "Afakihartwok@gmail.com"})
+	// sample := Sample{"Afakih"}
+	// sampleType := reflect.TypeOf(sample)
+	// structField := sampleType.Field(0)
+
+	// fmt.Println(structField.Name)
+}
